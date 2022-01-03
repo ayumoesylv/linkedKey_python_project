c871e73e732a71dc084b4adfceb01bbab0f90840
sylviazhang <sylvia.zhang88@outlook.com>
2022-01-02 21:57:26 -0500

    2021-10-26-01

--- Free Time Stuff/Just for fun/Name-Comment Game V2.py
+++ Free Time Stuff/Just for fun/Name-Comment Game V2.py
@@ -3,7 +3,7 @@ import random
 the_stinky_dictionary = {1: "ran away to Alberta.", 2: "is good at math.", 3: "is an amazing person.",
                          4: "should give me 100 dollars right now.", 5: "fell down the stairs.",
                          6: "threw up projectile vomit.", 7: "is going to be sacrificed to the blood god on Tuesday.",
-                         8: "is a vampire.", 9: "is going to be a billionaire.", 10: "owes me skittles.", 11: "has a secret crush on the previous person.", 12: "needs to take a shower."}
+                         8: "is a vampire.", 9: "is going to be a billionaire.", 10: "owes me skittles.", 11: "has a secret crush on someone.", 12: "needs to take a shower."}
 
 
 def play_random_comment():
